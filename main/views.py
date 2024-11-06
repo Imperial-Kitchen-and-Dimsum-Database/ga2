eb860988f6c69d9d145f1521eabe2e66d76b61a1
KeiraDiaz <nkeiradiaz@gmail.com>
2024-11-06 21:19:48 +0700

    main routed

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def show_main(request):
+
+    context = {
+        'name' : "Database Assignment"
+    }
+
+    return render(request, "main.html", context)
