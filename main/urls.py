dbb738fdaeebc90b50db645c6de26901924a5527
KeiraDiaz <nkeiradiaz@gmail.com>
2024-11-09 13:56:19 +0700

    progress on navbar, testimonials done, needs styling, main, service, subcategory made

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from main.views import show_main
+from main.views import show_main, service, subcategory
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('', include('mypay_service.urls')),
+    path('service/', service, name='service'),
+    path('subcategory/', subcategory, name='subcategory'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 
 def show_main(request):
-
     context = {
         'name' : "Database Assignment"
     }
-
     return render(request, "main.html", context)
+
+def service(request):
+    return render(request, "service_details.html")
+
+def subcategory(request):
+    return render(request, "subcategory_page.html")
