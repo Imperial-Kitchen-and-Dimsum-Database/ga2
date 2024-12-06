7d2a64c0dc441768d8bde3a4ea7ab41f6ab34ab6
KeiraDiaz <nkeiradiaz@gmail.com>
2024-12-06 14:04:06 +0700

    Promos and Vouchers already fetched

--- testi_vouchers/views.py
+++ testi_vouchers/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.db import connection
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
@@ -7,37 +6,52 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.db import connection
 
-from django.shortcuts import render
-from django.core.paginator import Paginator
-from django.db import connection
-
 def vouchers(request):
+    # Fetch vouchers
     with connection.cursor() as cursor:
         cursor.execute("""
             SELECT *
-            FROM "VOUCHER" 
+            FROM "VOUCHER" NATURAL JOIN "DISCOUNT"
         """)
         vouchers = cursor.fetchall()
 
     vouchers_list = []
     for row in vouchers:
         vouchers_list.append({
-            'voucher_id': row[0],
-            'voucher_code': row[1],
-            'discount_code': row[2],
-            'discount_percentage': row[1],
-            'service_name': row[1],
-            'minimum_transaction': row[1],
+            'voucher_code': row[0],
+            'discount_percentage': row[4],
+            'service_name': "SERVICES",
+            'minimum_transaction': row[5],
             'valid_days': row[1],
-            'user_quota': row[1],
+            'user_quota': row[2],
             'price': row[1],
         })
 
-    paginator = Paginator(vouchers_list, 3)  
+    # Fetch promos
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT *
+            FROM "PROMO" NATURAL JOIN "DISCOUNT"
+        """)
+        promos = cursor.fetchall()
+
+    promos_list = []
+    for row in promos:
+        promos_list.append({
+            'code': row[0],
+            'date': row[1],
+            'min_transaction': row[3],
+            'discount': row[2],
+
+        })
+
+    # Paginate vouchers
+    paginator = Paginator(vouchers_list, 3)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'vouchers.html', {'page_obj': page_obj})
+    # Pass both vouchers and promos to the template
+    return render(request, 'vouchers.html', {'page_obj': page_obj, 'promos_list': promos_list})
 
 # def purchase_voucher(request, voucher_id):
 #     user_id = request.session.get('user_id')  # Assuming session stores user_id
