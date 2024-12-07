916a7f9d90f4f9be2f63fca72ef9b0c045ec7c5e
isaacboentoro <isaac.jesse@ui.ac.id>
2024-12-07 12:25:35 +0700

    Fix worker service booking undefined transaction

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from functools import wraps
 from django.shortcuts import redirect
-from django.db import connection
+from django.db import connection, transaction  # Add transaction here
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
